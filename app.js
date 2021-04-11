46a197aa0c402fd98908523a904eb0ef224947ee
tuomakuh <70937274+tuomakuh@users.noreply.github.com>
2021-04-11 19:27:01 +0300

    'auths'

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const authRoute = require('./routes/authRoute');
 
+app.use('/auth', authRoute)
 app.use('/', express.static('public', {index: 'index.html'}));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
