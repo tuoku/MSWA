f32a0f0e070cff86a3823b2cf0ac89f1a33bab83
tuomakuh <70937274+tuomakuh@users.noreply.github.com>
2021-04-13 19:32:48 +0300

    'search'

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const app = express();
 const port = 3000;
 const cors = require('cors');
 const authRoute = require('./routes/authRoute');
+const userRoute = require('./routes/userRoute');
 
 app.use(cors());
 
 app.use('/auth', authRoute)
+app.use('/user', userRoute)
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
