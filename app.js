21ddc80cee2e9969b21d3e2f0d6b324633f0ee87
Kenert <kenert2001@gmail.com>
2021-04-14 21:36:48 +0300

    added app use post route

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const app = express();
 const port = 3000;
 const cors = require('cors');
 const authRoute = require('./routes/authRoute');
+const postRoute = require('./routes/postRoute');
 
 app.use(cors());
 
-app.use('/auth', authRoute)
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.json());
+
+app.use('/auth', authRoute);
+app.use('/post', postRoute);
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
