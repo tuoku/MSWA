7b6715dad2f1ee746a64073bb998c7d9036ca46f
Kenert <kenert2001@gmail.com>
2021-04-27 16:01:44 +0300

    sharp added, post picture cropping

--- app.js
+++ app.js
@@ -9,23 +9,22 @@ const authRoute = require('./routes/authRoute');
 const postRoute = require('./routes/postRoute');
 const userRoute = require('./routes/userRoute');
 
-app.enable('trust proxy');
-app.use ((req, res, next) => {
-  if (req.secure) {
-    next();
-  } else {
-    const proxypath = process.env.PROXY_PASS;
-    res.redirect(301, `https://${req.headers.host}${proxypath}${req.url}`);
-  }
-});
+// app.enable('trust proxy');
+// app.use ((req, res, next) => {
+//   if (req.secure) {
+//     next();
+//   } else {
+//     const proxypath = process.env.PROXY_PASS;
+//     res.redirect(301, `https://${req.headers.host}${proxypath}${req.url}`);
+//   }
+// });
 
 app.use(cors());
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/auth', authRoute);
 app.use('/post', postRoute);
+app.use('/thumbnails', express.static('thumbnails'));
 app.use('/uploads', express.static('./uploads'));
-app.use('/auth', authRoute);
-app.use('/user', userRoute);
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
